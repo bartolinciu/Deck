ab5d23a651806fc93b75cfb351d59b03efc10c83
Michał Ferenc <michael2001@poczta.onet.pl>
2023-12-20 12:59:35 +0100

    Remove unnecessary files from repository

--- Deck/actions/test.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from Deck.DeckController import DeckController
-
-@DeckController.action( label = "Test", parameters = [{"label": "Parameter", "type":"Boolean"}])
-async def test(device, parameters):
-	print(parameter)
